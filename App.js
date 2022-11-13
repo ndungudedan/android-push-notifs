8cc8ddc644a5ceb3a21205668c446451a9d94e03
ndungudedan <dnkibere@gmail.com>
2022-11-13 21:55:01 +0300

    take screenshot

--- App.js
+++ App.js
@@ -4,19 +4,17 @@ import { Alert, View, Text } from 'react-native';
 
 import messaging from '@react-native-firebase/messaging';
 
+async function getFCMToken() {
+	const token = await messaging().getToken();
+	console.log(token);
+}
 
 function App() {
-
-	const fcmToken = async () => {
-		if (fcmToken) {
-			const token = await messaging().getToken();
-			console.log(token);
-		}
-	}
+	getFCMToken();
 
 	useEffect(() => {
 		const unsubscribe = messaging().onMessage(async remoteMessage => {
-			Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+			Alert.alert('FCM Title: ' + remoteMessage.notification.title, 'FCM Body: ' + remoteMessage.notification.body);
 		});
 
 		return unsubscribe;
